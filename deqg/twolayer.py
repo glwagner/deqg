34cad3b8c5b64c9dfddda9dba6d69b08327973f8
Navid Constantinou <navidcy@gmail.com>
2018-12-23 07:00:23 -0800

    fixes typo in bottom-layer equation

--- deqg/twolayer.py
+++ deqg/twolayer.py
@@ -17,7 +17,7 @@ class TwoLayerModel(QGModel):
 
     Args
     ----
-        nx : (int) 
+        nx : (int)
             Grid resolution in :math:`x`
 
         ny : (int)
@@ -50,25 +50,25 @@ class TwoLayerModel(QGModel):
     def __init__(self,
         nx = 64,
         ny = 64,
-        Lx = 2.0,       
-        Ly = 2.0,       
-        F1 = 25.0,       
+        Lx = 2.0,
+        Ly = 2.0,
+        F1 = 25.0,
         F2 = 6.25,
         β = 0.0,
         ν = 0.0,
         U = "0",
         η = "0",
-        **params         
+        **params
         ):
 
         # Create bases and domain
         self.xbasis = xbasis = de.Fourier('x', nx, interval=(-Lx/2, Lx/2), dealias=3/2)
         self.ybasis = ybasis = de.Fourier('y', ny, interval=(-Ly/2, Ly/2), dealias=3/2)
         self.domain = domain = de.Domain([xbasis, ybasis], grid_dtype=np.float64)
-        
+
         self.variables = variables = ['q1', 'q2', 'ψ1', 'ψ2']
         self.problem = problem = de.IVP(domain, variables=variables, time='t')
-        
+
         add_parameters(problem, β=β, F1=F1, F2=F2, ν=ν, **params)
         bind_parameters(self, β=β, F1=F1, F2=F2, ν=ν, U=U, η=η, **params)
 
@@ -82,7 +82,7 @@ class TwoLayerModel(QGModel):
 
         # Equations
         problem.add_equation("dt(q1) + β*ψ1x + ν*lap(lap(q1)) = - J(ψ1, q1) + (Uyy - F1*U)*ψ1x - U*q1x")
-        problem.add_equation("dt(q2) + β*ψ2x + ν*lap(lap(q2)) = - J(ψ1, q1+η)      + F2*U*ψ2x")
+        problem.add_equation("dt(q2) + β*ψ2x + ν*lap(lap(q2)) = - J(ψ2, q2+η)      + F2*U*ψ2x")
 
         problem.add_equation("q1 - lap(ψ1) - F1*(ψ2-ψ1) = 0", condition="(nx != 0) or (ny != 0)")
         problem.add_equation("q2 - lap(ψ2) - F2*(ψ1-ψ2) = 0", condition="(nx != 0) or (ny != 0)")
@@ -91,13 +91,13 @@ class TwoLayerModel(QGModel):
 
         self.x = domain.grid(0)
         self.y = domain.grid(1)
-        
+
     def build_solver(self, timestepper='RK443'):
         """Build a dedalus solver for the model with `timestepper`.
 
         Args
         ----
-            timestepper : The name of the timestepper to be used by the solver. 
+            timestepper : The name of the timestepper to be used by the solver.
         """
         QGModel.build_solver(self, timestepper=timestepper)
         bind_state_variables(self, self.variables)
